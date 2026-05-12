5190b47d6f64140afc648d5a4faf8c195115fa90
Rounak Prasad <rounak16112006@gmail.com>
2026-05-12 18:26:49 +0530

    Complete User model

--- application/controllers.py
+++ application/controllers.py
@@ -1,2 +1,3 @@
 from flask import request, render_template, redirect, url_for
 from flask import current_app as app
+from .models import *
--- application/models.py
+++ application/models.py
@@ -1,1 +1,10 @@
 from .database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique = True, nullable = False)
+    email = db.Column(db.String, unique = True, nullable = False)
+    password = db.Column(db.String, unique = True, nullable = False)
+    role = db.Column(db.String, nullable = False, default = 'customer')
+
+    
