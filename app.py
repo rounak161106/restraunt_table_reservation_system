899fab8843c25a379979e48c6c724c351d0891f5
Rounak Prasad <rounak16112006@gmail.com>
2026-05-12 18:15:04 +0530

    Complete basic config

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from application.database import db
 app = None
 
 def create_app():
     app = Flask()
     app.debug = True
-    
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///e-dine.sqlite3'
+    db.init_app(app)
+    app.app_context().push()
+    return app
+
+app = create_app()
+
+from application.controllers import *
+
+if __name__ == "__main__":
+    app.run()
---
+++ application/controllers.py
@@ -0,0 +1,2 @@
+from flask import request, render_template, redirect, url_for
+from flask import current_app as app
