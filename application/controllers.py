024696ebc1488df693c47762bec61a05a88ee277
Rounak Prasad <rounak16112006@gmail.com>
2026-05-12 17:31:20 +0530

    Restart the project

--- application/controllers.py
+++
@@ -1,7 +0,0 @@
-from flask import current_app as app
-from .models import *
-from flask import Flask, render_template, request, url_for
-
-@app.route('/')
-def main():
-    return render_template("login.html")
