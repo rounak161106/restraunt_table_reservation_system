655cbe8708d96eeb95a2a2bd4ca2a729a9b1a7cf
Rounak Prasad <rounak16112006@gmail.com>
2026-05-12 19:27:32 +0530

    Create login route

--- application/controllers.py
+++ application/controllers.py
@@ -2,3 +2,25 @@ from flask import request, render_template, redirect, url_for
 from flask import current_app as app
 from .models import *
 
+@app.route('/')
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        this_user = User.query.filter_by(username = username).first()
+        if this_user:
+            if this_user.password == password:
+                if this_user.role == 'manager':
+                    return redirect('/manager')
+                return redirect(f'/user/{this_user.id}')
+            else:
+                return "Incorrect Password"
+        else:
+            return "User doesn't exists!!"
+    return render_template('login.html')
+        
+@app.route('/manager')
+def manager_dash():
+    return "manager"
+    
